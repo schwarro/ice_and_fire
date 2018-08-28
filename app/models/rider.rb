5189149662a64f21f16acb44893003a5b5aad586
schwarro <rob.ap.schwartz@gmail.com>
2018-08-28 12:10:59 -0400

    completed dynamic attributes in the rider factory section

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -3,7 +3,7 @@ class Rider < ApplicationRecord
   has_many :dragons
 
   validates :nickname, presence: true
-  # validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 
   def swing
     "#{name} brings #{sword} crashing down!"
--- test/factories/riders.rb
+++ test/factories/riders.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :rider do
     nickname 'The Conqueror'
+    email {"#{name.downcase}@westeros.com"}
   end
 end
